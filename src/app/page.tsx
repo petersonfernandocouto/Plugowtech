6b720f56ce5e3e97dd55d07204b99e4b46ee92c6
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2024-12-12 17:28:41 -0300

    removido duplicata de codigo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,46 +6,11 @@ import Points from "../../Componentes/Points";
 import PointsMobile from "../../Componentes/PointsMobile";
 import Invitation from "../../Componentes/Invitation";
 import Footer from "../../Componentes/Footer";
-//import Faq from "../../Componentes/Faq"
 import Loader from "../../Componentes/Loader";
 import { useEffect, useState } from "react";
-
 import { useInView } from "react-intersection-observer";
 
 export default function Home() {
-        const { ref: PremiosRef, inView: PremiosInView } = useInView({
-          triggerOnce: true,
-        });
-        const { ref: PointsRef, inView: PointsInView } = useInView({
-          triggerOnce: true,
-        });
-        const { ref: PointsMobileRef, inView: PointsMobileInView } = useInView({
-          triggerOnce: true,
-        });
-        const { ref: InvitationRef, inView: InvitationInView } = useInView({
-          triggerOnce: true,
-        });
-
-        const { ref: FooterRef, inView: FooterInView } = useInView({
-          triggerOnce: true,
-        });
-
-
-        const [isLoading, setIsLoading] = useState(true);
-
-        useEffect(() => {
-          // Aqui estou simulando um atraso no carregamento para exibir o loader
-          const timer = setTimeout(() => {
-            setIsLoading(false);
-          }, 2000); // tempo de 2 segundos de espera
-
-          return () => clearTimeout(timer);
-        }, []);
-
-        if (isLoading) {
-          return <Loader />;
-        }
-
   const { ref: PremiosRef, inView: PremiosInView } = useInView({
     triggerOnce: true,
   });
@@ -58,10 +23,23 @@ export default function Home() {
   const { ref: InvitationRef, inView: InvitationInView } = useInView({
     triggerOnce: true,
   });
-
   const { ref: FooterRef, inView: FooterInView } = useInView({
     triggerOnce: true,
   });
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulando atraso no carregamento
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // tempo de 2 segundos
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <>
@@ -70,96 +48,50 @@ export default function Home() {
       </div>
 
       <div
-        id="Premios"
+        id="PremiosSection"
         ref={PremiosRef}
         className={`transition-all duration-1000 ease-out ${
-          PremiosInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
-        }`}
-      >
-      <div
-        id="Premios"
-        ref={PremiosRef}
-        className={`transition-all duration-1000 ease-out ${
-          PremiosInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
+          PremiosInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
         }`}
       >
         <Premios />
       </div>
 
       <div
-        id="Points"
+        id="PointsSection"
         ref={PointsRef}
         className={`transition-all duration-1000 ease-out hidden lg:block ${
-          PointsInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
-        }`}
-      >
-      <div
-        id="Points"
-        ref={PointsRef}
-        className={`transition-all duration-1000 ease-out hidden lg:block ${
-          PointsInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
+          PointsInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
         }`}
       >
         <Points />
       </div>
 
       <div
-        id="PointsMobile"
+        id="PointsMobileSection"
         ref={PointsMobileRef}
         className={`transition-all duration-1000 ease-out lg:hidden ${
-          PointsMobileInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
-        }`}
-      >
-      <div
-        id="PointsMobile"
-        ref={PointsMobileRef}
-        className={`transition-all duration-1000 ease-out lg:hidden ${
-          PointsMobileInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
+          PointsMobileInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
         }`}
       >
         <PointsMobile />
       </div>
 
       <div
-        id="Invitation"
+        id="InvitationSection"
         ref={InvitationRef}
         className={`transition-all duration-1000 ease-out ${
-          InvitationInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
+          InvitationInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
         }`}
       >
         <Invitation />
       </div>
 
       <div
-        id="Footer"
+        id="FooterSection"
         ref={FooterRef}
         className={`transition-all duration-1000 ease-out ${
-          FooterInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
-        }`}
-      >
-      <div
-        id="Footer"
-        ref={FooterRef}
-        className={`transition-all duration-1000 ease-out ${
-          FooterInView
-            ? "opacity-100 translate-y-0"
-            : "opacity-0 translate-y-20"
+          FooterInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
         }`}
       >
         <Footer />
